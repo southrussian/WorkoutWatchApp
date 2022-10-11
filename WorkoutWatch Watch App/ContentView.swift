84adb88e305f0bee8cd3a928d2d3735967af3f58
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-10-11 10:55:13 +0300

    Initial Commit

---
+++ WorkoutWatch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WorkoutWatch Watch App
+//
+//  Created by Danil Peregorodiev on 11.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkoutWatch Watch App/WorkoutWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutWatchApp.swift
+//  WorkoutWatch Watch App
+//
+//  Created by Danil Peregorodiev on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
