50175fa8d120de0c078690fa195508abda0837b6
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-03-25 23:16:34 +0300

    Created UI for statuses: inProgress, paused, completed

--- WorkoutWatch Watch App/ContentView.swift
+++ WorkoutWatch Watch App/ContentView.swift
@@ -9,8 +9,190 @@ import SwiftUI
 import HealthKit
 
 struct ContentView: View {
+    @ObservedObject var workoutsession = WorkoutSession()
+    @State private var scale = 1.0
     var body: some View {
-        Text("Workout")
+        VStack {
+            if workoutsession.status == .notStarted {
+                Button("Начать тренировку") {
+                    workoutsession.setupSession()
+                    workoutsession.startWorkoutSession()
+                }
+            } else if workoutsession.status == .inProgress {
+                if workoutsession.distance != nil {
+                    HStack {
+                        Image(systemName: "ruler")
+                        Spacer()
+                        Text(distanceFormatter())
+                    }
+                }
+                
+                if workoutsession.bpm != nil {
+                    HStack {
+                        Image(systemName: "heart")
+                            .foregroundColor(.red)
+                            .scaleEffect(scale)
+                            .onAppear {
+                                let animation = Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
+                                withAnimation(animation) {
+                                    scale = 0.5
+                                }
+                            }
+                        Spacer()
+                        Text(heartBeatFormatter())
+                    }
+                }
+                
+                if workoutsession.energyBurned != nil {
+                    HStack {
+                        Text("\(workoutsession.energyBurned!) ккал")
+                        Spacer()
+                    }
+                }
+                
+                if workoutsession.elapsedTime != nil {
+                    HStack {
+                        Image(systemName: "stopwatch")
+                        Spacer()
+                        Text("\(workoutsession.elapsedTime!)")
+                    }
+                }
+                
+                Button {
+                    workoutsession.pauseWorkout()
+                    scale = 1.0
+                } label: {
+                    HStack {
+                        Image(systemName: "pause")
+                            .padding(5)
+                        Text("Пауза")
+                    }
+                }
+                
+                Button {
+                    workoutsession.endWorkoutSession()
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "stop.fill")
+                            .padding(5)
+                        Text("Закончить тренировку")
+                    }
+                }
+            } else if workoutsession.status == .paused {
+                if workoutsession.distance != nil {
+                    HStack {
+                        Image(systemName: "ruler")
+                        Spacer()
+                        Text(distanceFormatter())
+                    }
+                }
+                
+                if workoutsession.bpm != nil {
+                    HStack {
+                        Image(systemName: "heart")
+                            .foregroundColor(.red)
+                            .scaleEffect(scale)
+                            .onAppear {
+                                let animation = Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
+                                withAnimation(animation) {
+                                    scale = 0.5
+                                }
+                            }
+                        Spacer()
+                        Text(heartBeatFormatter())
+                    }
+                }
+                
+                if workoutsession.energyBurned != nil {
+                    HStack {
+                        Text("\(workoutsession.energyBurned!) ккал")
+                        Spacer()
+                    }
+                }
+                
+                if workoutsession.elapsedTime != nil {
+                    HStack {
+                        Image(systemName: "stopwatch")
+                        Spacer()
+                        Text("\(workoutsession.elapsedTime!)")
+                    }
+                }
+                
+                Button {
+                    workoutsession.resumeWorkout()
+                    scale = 1.0
+                } label: {
+                    HStack {
+                        Image(systemName: "play.circle")
+                            .padding(5)
+                        Text("Продолжить тренировку")
+                    }
+                }
+                
+                Button {
+                    workoutsession.endWorkoutSession()
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "stop.fill")
+                            .padding(5)
+                        Text("Закончить тренировку")
+                    }
+                }
+            } else if workoutsession.status == .complete {
+                if workoutsession.distance != nil {
+                    HStack {
+                        Image(systemName: "ruler")
+                        Spacer()
+                        Text(distanceFormatter())
+                    }
+                }
+                
+                if workoutsession.bpm != nil {
+                    HStack {
+                        Image(systemName: "heart")
+                            .foregroundColor(.red)
+                            .scaleEffect(scale)
+                            .onAppear {
+                                let animation = Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
+                                withAnimation(animation) {
+                                    scale = 0.5
+                                }
+                            }
+                        Spacer()
+                        Text(heartBeatFormatter())
+                    }
+                }
+                
+                if workoutsession.energyBurned != nil {
+                    HStack {
+                        Text("\(workoutsession.energyBurned!) ккал")
+                        Spacer()
+                    }
+                }
+                
+                if workoutsession.elapsedTime != nil {
+                    HStack {
+                        Image(systemName: "stopwatch")
+                        Spacer()
+                        Text("\(workoutsession.elapsedTime!)")
+                    }
+                }
+            }
+        }
+    }
+    
+    func distanceFormatter() -> String {
+        let formatter = NumberFormatter()
+        formatter.maximumSignificantDigits = 3
+        return formatter.string(from: workoutsession.distance! as NSNumber)!
+    }
+    
+    func heartBeatFormatter() -> String {
+        let formatter = NumberFormatter()
+        formatter.maximumSignificantDigits = 0
+        return formatter.string(from: workoutsession.bpm! as NSNumber)!
     }
 }
 
